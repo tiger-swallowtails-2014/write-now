1aaf5edea9eeffbe83ddbf469c606dd53484610f
Jared_C <brasschaos@gmail.com>
2014-09-27 15:43:27 -0700

    Adds BCrypt password reader and writer functionality to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
+  include BCrypt
   validates :email, :password_hash, presence: true
   validates :email, uniqueness: true
   has_many :projects
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
