6e2d2a8fcce9968eb9bf2f693ca062bbdd31335c
Jared_C <brasschaos@gmail.com>
2014-09-27 15:57:13 -0700

    Add create action functionality to users_controller. Add private method user_params for strong parameter implementation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    @user.password = params[:password]
+    @user.save!
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
