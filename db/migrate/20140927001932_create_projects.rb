39842090906f2263486c6186281b65cdd1549c6f
Cfeusier <cfeusier@gmail.com>
2014-09-26 17:46:24 -0700

    Add projects table migration constraints and Project model validations.

--- db/migrate/20140927001932_create_projects.rb
+++ db/migrate/20140927001932_create_projects.rb
@@ -1,12 +1,12 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.string :title
-      t.integer :wordcount_goal
+      t.string :title, length: 130, null: false
+      t.integer :wordcount_goal, null: false
       t.integer :goal_time_limit
       t.date :goal_deadline_date
-      t.boolean :active
-      t.integer :current_wordcount
+      t.boolean :active, null: false, default: true
+      t.integer :current_wordcount, null: false, default: 0
 
       t.timestamps
     end
