63334627274b7dca8e4e5c8d6bf035076b830e24
Cfeusier <cfeusier@gmail.com>
2014-09-28 14:15:59 -0700

    Add SessionsController spec with outline descriptions for #new, #create, and #destroy controller actions.

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe SessionsController do
+  context '#new' do
+    it "GET :new"
+  end
+
+  context '#create' do
+    it "POST #create"
+  end
+
+  context '#destroy' do
+    it "DELETE #destroy"
+  end
+
+end
