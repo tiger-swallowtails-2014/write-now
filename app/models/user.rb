87f2ea095a8b6761fb51f7d15424dc55894bb4b7
Cfeusier <cfeusier@gmail.com>
2014-09-27 20:42:33 -0700

    Add has_secure_password to User model for newer Rails-Bcrypt integration. Remove unneeded Bcrypt User model methods. Change User model validation to check :password_digest instead of :password_hash.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,7 @@
 class User < ActiveRecord::Base
-  include BCrypt
-  validates :email, :password_hash, presence: true
+  has_secure_password
+  validates :email, :password_digest, presence: true
   validates :email, length: { minimum: 1 }
   validates :email, uniqueness: true
   has_many :projects
-
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
-
 end
