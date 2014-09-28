f76a40e21857b1e78f112450fc4cb12bf2044f79
Cfeusier <cfeusier@gmail.com>
2014-09-28 16:47:02 -0700

    Reimplement #create action on ProjectsController. Implement conditional save and error messaging.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,20 +1,24 @@
 class ProjectsController < ApplicationController
-
   def new
     @user = User.find(params[:user_id])
     @project = Project.new
   end
 
-  # def create
-  #   @user = User.find(params[:user_id])
-  #   @project = Project.new(project_params)
-  #   @user.save!
-  #   redirect_to root_path
-  # end
+  def create
+    @user = User.find(params[:user_id])
+    @project = Project.new(project_params)
 
-  # private
-    # def project_params
-    #   params.require(:project).permit(:title, :wordcount_goal, :goal_time_limit, :goal_deadline_date)
-    # end
+    if @project.save
+      redirect_to root_path
+      # TODO: write conditional rendering check on global controller index action
+    else
+      flash.now[:error] = "Learn how to type ... if you want to be a writer."
+      render :new
+    end
+  end
 
+  private
+    def project_params
+      params.require(:project).permit(:title, :wordcount_goal, :goal_time_limit, :goal_deadline_date, :user_id)
+    end
 end
