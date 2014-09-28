a2e69e23f7f11dfb9ea94b3a0b78aed32b863fb3
Cfeusier <cfeusier@gmail.com>
2014-09-27 20:59:59 -0700

    Formatting and whitespace updates.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,7 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
-
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
