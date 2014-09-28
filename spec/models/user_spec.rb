f66b1f41839d88ae71bb552df8d4f6bfe694913b
Cfeusier <cfeusier@gmail.com>
2014-09-27 20:44:35 -0700

    Change all occurences of :password_hash to :password_digest within User specs (models and controllers) and User factory.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User do
     it "is valid with an email and password hash" do
       user = User.new(
         email: "me@me.com",
-        password_hash: "god")
+        password_digest: "god")
       expect(user).to be_valid
     end
 
@@ -14,13 +14,13 @@ describe User do
     end
 
     it "is invalid with a duplicate email" do
-      User.create(email: "awesome@me.com", password_hash: "something")
-      user = User.new(email: "awesome@me.com", password_hash: "something")
+      User.create(email: "awesome@me.com", password_digest: "something")
+      user = User.new(email: "awesome@me.com", password_digest: "something")
       expect(user).to have(1).errors_on(:email)
     end
 
-    it "is invalid without an password hash" do
-      expect(User.new(password_hash: nil)).to have(1).errors_on(:password_hash)
+    it "is invalid without an password digest" do
+      expect(User.new(password_digest: nil)).to have(1).errors_on(:password_digest)
     end
   end
 
