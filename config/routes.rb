62fa03a6d7b1f75bf2704e6f847611e8aac341f4
Cfeusier <cfeusier@gmail.com>
2014-09-27 20:23:25 -0700

    Add sessions (login) routes to routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # root to #index action of global controller
   root 'welcome#index'
 
+  # login (sessions) routes with named helpers
+  get '/login' => 'sessions#new', as: 'login_form'
+  post '/login' => 'sessions#create', as: 'login'
+  destroy '/logout' => 'sessions#destroy', as: 'logout'
+
+  # nested users and projects resources
   resources :users do
     resources :projects
   end
