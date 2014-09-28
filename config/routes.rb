0d4b1de5201d670b726365a7e3bb1b44b06091ef
Cfeusier <cfeusier@gmail.com>
2014-09-27 20:24:47 -0700

    Add 2 'named' routes to make user paths cleaner throughout views. Signup_form and signup are now url/path helpers for users#new and users#create, respectively.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create', as: 'login'
   destroy '/logout' => 'sessions#destroy', as: 'logout'
 
+  # user routes with named helpers (just #new and #create actions)
+  get '/users/new' => 'users#new', as: 'signup_form'
+  post '/users' => 'users#create', as: 'signup'
+
   # nested users and projects resources
   resources :users do
     resources :projects
