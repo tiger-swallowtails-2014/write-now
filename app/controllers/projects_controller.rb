f388e25e9779479f4b41b9d7d5814a9ef9fcc98a
Cfeusier <cfeusier@gmail.com>
2014-09-28 16:21:06 -0700

    Add create mvp action in ProjectsController.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,16 +5,16 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
-  def create
-    @user = User.find(params[:user_id])
-    @project = Project.new(project_params)
-    @user.save!
-    redirect_to root_path
-  end
+  # def create
+  #   @user = User.find(params[:user_id])
+  #   @project = Project.new(project_params)
+  #   @user.save!
+  #   redirect_to root_path
+  # end
 
-  private
-  def project_params
-    params.require(:project).permit(:title, :wordcount_goal, :goal_time_limit, :goal_deadline_date)
-  end
+  # private
+    # def project_params
+    #   params.require(:project).permit(:title, :wordcount_goal, :goal_time_limit, :goal_deadline_date)
+    # end
 
 end
