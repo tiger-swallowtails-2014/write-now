55885a573c33d364e7e9a39006716ab5eb0a201e
Cfeusier <cfeusier@gmail.com>
2014-09-27 20:26:18 -0700

    Change incorrect route mapper from 'destroy' to 'delete.'

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # login (sessions) routes with named helpers
   get '/login' => 'sessions#new', as: 'login_form'
   post '/login' => 'sessions#create', as: 'login'
-  destroy '/logout' => 'sessions#destroy', as: 'logout'
+  delete '/logout' => 'sessions#destroy', as: 'logout'
 
   # user routes with named helpers (just #new and #create actions)
   get '/users/new' => 'users#new', as: 'signup_form'
