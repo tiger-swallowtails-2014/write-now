9ead8e4d660e340fef48c7265b851a68c8051c0f
Cfeusier <cfeusier@gmail.com>
2014-09-27 20:50:51 -0700

    Remove unneeded #password Bcrypt helper call in user #create action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # @user.password = params[:password]
     @user.save!
     redirect_to root_path
   end
