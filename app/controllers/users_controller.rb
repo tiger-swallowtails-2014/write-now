5feb3102452e39c513e39c9ea07913de25a68f7e
Cfeusier <cfeusier@gmail.com>
2014-09-28 18:10:06 -0700

    Add #edit and #update actions to UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(email: user_params[:email])
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def destroy
