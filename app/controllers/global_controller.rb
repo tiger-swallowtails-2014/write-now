ec2631bc119d4526c31c6e5122452fd0f4fa7b07
Cfeusier <cfeusier@gmail.com>
2014-09-29 08:34:06 -0700

    Refactor GlobalController #index action into 'displayer' methods.

--- app/controllers/global_controller.rb
+++ app/controllers/global_controller.rb
@@ -3,24 +3,27 @@ class GlobalController < ApplicationController
     if !current_user
       render "_squeeze"
     else
-      if active_project
-        @user = current_user
-        @project = @user.projects.last
-        render "projects/_current_project"
-      else
-        @user = current_user
-        @project = Project.new
-        render "projects/_new"
-      end
+      @user = current_user
+      active_project ? active_project_displayer : new_project_displayer
     end
   end
 
   private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
 
-  def active_project
-    current_user.projects.last.active if current_user.projects.any?
-  end
+    def active_project
+      current_user.projects.last.active if current_user.projects.any?
+    end
+
+    def new_project_displayer
+      @project = Project.new
+      render "projects/_new"
+    end
+
+    def active_project_displayer
+      @project = @user.projects.last
+      render "projects/_current_project"
+    end
 end
