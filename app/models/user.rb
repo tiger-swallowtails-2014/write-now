e0248f954331b3b82bb0a2150046e1462532ebb8
Cfeusier <cfeusier@gmail.com>
2014-09-29 09:28:38 -0700

    Refactor User model to have associations above validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :projects
   validates :email, :password_digest, presence: true
   validates :email, length: { minimum: 1 }
   validates :email, uniqueness: true
-  has_many :projects
 end
