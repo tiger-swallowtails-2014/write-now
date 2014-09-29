60e1f9ebb5e9fe3c2f11262953468916780546fa
Cfeusier <cfeusier@gmail.com>
2014-09-28 17:54:08 -0700

    Add stub #edit and #destroy actions in the UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+  end
+
+  def destroy
+  end
+
   private
     def user_params
       params.require(:user).permit(:email, :password)
