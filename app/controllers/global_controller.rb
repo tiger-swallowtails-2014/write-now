f50de2cd40c32018c2a126b7f7571d180dd9736b
Cfeusier <cfeusier@gmail.com>
2014-09-28 20:44:38 -0700

    Fix rebase conflict to include refactoring of GlobalController #index action and private methods.

--- app/controllers/global_controller.rb
+++ app/controllers/global_controller.rb
@@ -1,31 +1,26 @@
 class GlobalController < ApplicationController
   def index
-  	if current_user && active_project
-  		@user = current_user
-  		@project = @user.projects.last
-	  	render "projects/_current_project"
-	  elsif current_user && (active_project == false)
-	  	@user = current_user
-	  	@project = Project.new
-	  	render "projects/_new"
-  	else
-  		render "_squeeze"
-	  end
+    if !current_user
+      render "_squeeze"
+    else
+      if active_project
+        @user = current_user
+        @project = @user.projects.last
+        render "projects/_current_project"
+      else
+        @user = current_user
+        @project = Project.new
+        render "projects/_new"
+      end
+    end
   end
 
   private
-	  def current_user
-	  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
-	  end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
-	  def active_project
-	  	if current_user
-	  		projects = current_user.projects
-		  	if projects.empty?
-		  		false
-		  	else
-		  		projects.last.active
-		  	end
-		  end
-	  end
+  def active_project
+    current_user.projects.last.active if current_user.projects.any?
+  end
 end
