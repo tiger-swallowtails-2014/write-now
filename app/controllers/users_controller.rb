ac1ad496248f82cef294466452e5897de4f8d469
Michele Murphy <mmichelemurphy@gmail.com>
2014-10-01 11:29:04 -0700

    Update :user_id to be :id to reflect Rails routing conventions. * Update users, sessions and application controller as well as users_helper.rb * Test sign-up, sign-in, update and delete features.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
+      session[:id] = user.id
       redirect_to root_path
     else
       flash[:error] = "Invalid account information. Try signing up again with different information."
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
 
   def destroy
     if current_user.destroy
-      session[:user_id] = nil
+      session[:id] = nil
       redirect_to root_path
     else
       flash.now[:error] = "Account Deletion failed! Please try again."
