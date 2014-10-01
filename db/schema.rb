50d32ea01a6bf24bdce4fee0004eaf81bc982392
Jared_C <brasschaos@gmail.com>
2014-10-01 09:29:49 -0700

    post initial migration commit on new branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141001032004) do
-=======
-ActiveRecord::Schema.define(version: 20140930210932) do
->>>>>>> Whitespace changes to schema file
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
