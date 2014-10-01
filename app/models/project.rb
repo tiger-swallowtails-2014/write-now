6adf6afeeb6b04e23005b50d3bbb5e52d805d89e
Jared_C <brasschaos@gmail.com>
2014-10-01 09:49:32 -0700

    Updates project model to include relation to milestone model.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+  has_many :milestones
   validates :title, :wordcount_goal, :current_wordcount, presence: true
   validates :title, length: { in: 1..130 }
   validates :wordcount_goal, :current_wordcount, numericality: true
